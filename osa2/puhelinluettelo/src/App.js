29aa6912197490dcf751a943bdeba7a31d2253d8
juhanikat <juhani.kataja@gmail.com>
2023-03-31 15:03:10 +0300

    2.11 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Filter = (props) => {
@@ -37,17 +38,13 @@ const DisplayPersons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState("")
   const [acceptedPersons, setAcceptedPersons] = useState([])
+
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -80,6 +77,16 @@ const App = () => {
     setAcceptedPersons(result)
     setNewFilter(event.target.value)
   }
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }, [])
 
   return (
     <div>
